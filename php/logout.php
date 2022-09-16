caa8da7a7eb8a72a8000c2505b382b09fee16473
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-16 08:36:59 +0500

    Logout Functionality

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_GET['logout']) && $_GET['logout'] == "true"){
         session_start();
-        
+        session_unset();
         session_destroy();
         header("Location: ../index.php");
         
