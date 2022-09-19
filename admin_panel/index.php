4faf0083c8a692022b3bc9a36b3097cd5a094ab9
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-20 01:29:27 +0500

    Admin Dashboard Panel

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -234,6 +234,8 @@
                         </div>
                     </div>
                 </main>
+
+                
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
