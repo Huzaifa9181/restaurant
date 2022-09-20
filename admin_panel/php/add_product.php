b11060084046bd7b69ca1017ef1079f63c0ad1f9
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-21 04:59:00 +0500

    add product modal handle functionality

---
+++ admin_panel/php/add_product.php
@@ -0,0 +1,33 @@
+<?php
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $name = $_POST['add_name'];
+    $price = $_POST['price'];
+    $cat = $_POST['cat'];
+    $img = $_FILES['upload'];
+  
+        
+    // echo "<pre>";
+    // echo print_r($img);
+    // echo "</pre>";
+    
+    $file_name = $_FILES['upload']['name'];
+    $file_path = $_FILES['upload']['full_path'];
+    $file_type =  $_FILES['upload']['type'];
+    $file_tmp = $_FILES['upload']['tmp_name'];
+    $file_error = $_FILES['upload']['error'];
+    $file_size = $_FILES['upload']['size'];
+  
+    if(move_uploaded_file($file_tmp,"../uploads/".$file_name)){
+        include "database.php";
+        $sql = "INSERT INTO `products` (`name`, `path`, `price`, `cat_id`) VALUES ('$name', '$file_name', '$price', '$cat');
+        ";
+        $result = mysqli_query($conn,$sql);
+    //   header("location: products.php?add_product=true");
+
+    }else{
+      header("location: products.php?add_product=false");
+    }
+  
+
+  }  
+?>
