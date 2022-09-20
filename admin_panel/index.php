e491c911da6a9242f07fe42ea104ea052b6aaba3
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-21 04:58:34 +0500

    Admin Dashboard Panel

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -145,18 +145,18 @@
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Product Sale</div>
+                                    <div class="card-body">Product Categories</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="php/category.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                                <div class="card bg-secondary text-white mb-4">
+                                    <div class="card-body">Products</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="php/products.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
