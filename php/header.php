982c50795f2f4568292e6edc9dd4a42f9b5b7bc3
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-23 22:11:56 +0500

    navigation bar

--- php/header.php
+++ php/header.php
@@ -1,39 +1,41 @@
-<header id="header" class="header fixed-top d-flex align-items-center">
-        <div class="container d-flex align-items-center justify-content-between">
+<div class="container mb-5">
+    <header id="header" class="header fixed-top d-flex align-items-center">
+            <div class="container d-flex align-items-center justify-content-between">
 
-            <a href="../index.php" class="logo d-flex align-items-center me-auto me-lg-0">
-                <!-- Uncomment the line below if you also wish to use an image logo -->
-                <!-- <img src="assets/img/logo.png" alt=""> -->
-                <h1>Yummy<span>.</span></h1>
-            </a>
+                <a href="../index.php" class="logo d-flex align-items-center me-auto me-lg-0">
+                    <!-- Uncomment the line below if you also wish to use an image logo -->
+                    <!-- <img src="assets/img/logo.png" alt=""> -->
+                    <h1>Yummy<span>.</span></h1>
+                </a>
 
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a href="../index.php">Home</a></li>
+                <nav id="navbar" class="navbar">
+                    <ul>
+                        <li><a href="../index.php">Home</a></li>
+                        <?php
+                        if(isset($_SESSION['Loggedin']) && $_SESSION['Loggedin'] == "true"){
+                            echo'                                                  
+                            <li><a href="table_reservation.php">Table Reservation</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </nav>
+                <!-- .navbar -->
+                <div>
                     <?php
-                    if(isset($_SESSION['Loggedin']) && $_SESSION['Loggedin'] == "true"){
-                        echo'                                                  
-                        <li><a href="table_reservation.php">Table Reservation</a></li>';
-                    }
+                        if(!isset($_SESSION['Loggedin'])){
+                            echo'
+                            <a class="btn-book-a-table" data-bs-toggle="modal" data-bs-target="#loginModal">Log In</a>
+                            <a class="btn-book-a-table" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</a>    
+                            ';
+
+                        }else{
+
+                            echo '<a href="logout.php?logout=true" class="btn-book-a-table">Logout</a>';
+                        }    
                     ?>
-                </ul>
-            </nav>
-            <!-- .navbar -->
-            <div>
-                <?php
-                    if(!isset($_SESSION['Loggedin'])){
-                        echo'
-                        <a class="btn-book-a-table" data-bs-toggle="modal" data-bs-target="#loginModal">Log In</a>
-                        <a class="btn-book-a-table" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</a>    
-                        ';
-
-                    }else{
-
-                        echo '<a href="logout.php?logout=true" class="btn-book-a-table">Logout</a>';
-                    }    
-                ?>
+                </div>
+                <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+                <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
             </div>
-            <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-        </div>
-    </header>
+        </header>
+</div>
