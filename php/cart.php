7e8b61abb42db1fa2f3c2ebcc49874eca782c1fa
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-24 06:36:48 +0500

    all orders fetch functionality

--- php/cart.php
+++ php/cart.php
@@ -48,30 +48,48 @@
                         <?php
                 
                     $number = 1;
+
                     if(isset($_SESSION['add_to_cart']) && !empty($_SESSION['add_to_cart'])){
                         foreach($_SESSION['add_to_cart'] as $key => $value ){
                             $sql = "SELECT * FROM `products` WHERE id =$value[id];";
                             $result = mysqli_query($conn,$sql);
                             $row = mysqli_fetch_assoc($result);
 
-                            // $total = 0;
-                            // $price= $row['price'];
+                            // $total;
+                            // $price= $value['price'];
                             // $total = $total + $price;
 
                             echo'<tr>
                             <td>'.$number.'</td>
-                            <td>'.$row['name'].'</td>
-                            <td>'.$row['price'].'</td>
-                            <td><input type="number" style="width: 36px;" value="1" class="quantity text-center"></td>
+                            <td>'.$value['name'].'</td>
+                            <td>'.$value['price'].'</td>
+                            
+                            
+                            <form method="post" action="add_to_cart.php">
+                                <div class="input-group mb-3">
+                                    <td>
+                                    <span style="display: flex;">
+                                    <button class="input-group-text decrement-btn">-</button>
+                                    <input type="number" style="width: 36px;" value="'.$value['quantity'].'" class="quantity inp-quantity text-center" onchange="this.form.submit();" name="quantity">
+                                    <button class="input-group-text increment-btn">+</button>
+                                    </span>
+                                    </td>
+
+                                    <input type="hidden" name="quantity_id" value="'.$value['id'].'">
+                                </div>
+                            </form>
                             
                             <td>
                                 <form method="post" action="add_to_cart.php">
                                     <button type="submit" class="btn btn-danger"><i class="bi bi-trash"></i></button>
                                     <input type="hidden" name="del_id" value="'.$value['id'].'">
+                                
                                 </form>
                             </td>
                             </tr>';
                             $number = $number + 1;
+
+                            
                         }
                     }    
                 ?>
@@ -83,8 +101,8 @@
             
                 <div class="pay">
                     <h3 class="text-center p-3">Total Amount    </h3>
-                    <h5 class="text-center">$<?php echo $total=0; ?></h5>
-                    <button class="btn btn-primary m-2" id="pay-btn">Check Out</button>
+                    <h5 class="text-center">$<?php echo $total = 0; ?></h5>
+                    <a href="insert_cart.php" class="btn btn-primary m-2" id="pay-btn">Check Out</a>
                 </div>
             </div>
         </div>
@@ -100,8 +118,36 @@
             $("#pay-btn").on("click",function(){
                 var data = $(".quantity").val();
                 console.log(data);
+            });
+
+            $(".increment-btn").click(function(e){
+                e.preventDefault();
+                var qty = $(this).closest(".table-cont").find(".inp-quantity").val();
+
+                var value = parseInt(qty, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value < 10){
+                    value ++;
+                    $(".inp-quantity").val(value);
+                    $(this).closest(".table-cont").find(".inp-quantity").val();
+                }
+            })
+
+            $(".decrement-btn").click(function(e){
+                e.preventDefault();
+                var qty = $(this).closest(".table-cont").find(".inp-quantity").val();
+
+                var value = parseInt(qty, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value > 1){
+                    value --;
+                    $(".inp-quantity").val(value);
+                    $(this).closest(".table-cont").find(".inp-quantity").val();
+                }
             })
         })
+
+
     </script>
 </body>
 
