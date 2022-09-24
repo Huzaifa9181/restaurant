203066e9da94df40a29aaaf5f3c45c150aa0fe4e
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-25 03:34:38 +0500

    all users orders fetch functionality

--- admin_panel/php/order_product.php
+++ admin_panel/php/order_product.php
@@ -61,7 +61,7 @@
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
-                    <li><a class="dropdown-item" href="php/logout.php?logout=true">Logout</a></li>
+                    <li><a class="dropdown-item" href="logout.php?logout=true">Logout</a></li>
                 </ul>
             </li>
         </ul>
