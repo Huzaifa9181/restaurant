23fa21db9c105ceec220d085145d7d84c344ec1c
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-25 21:24:19 +0500

    Admin Dashboard Panel

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -23,6 +23,7 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
